fee7909a948f4b32f06894bba253729766859022
Alex Holder <s2031787@ed.ac.uk>
2023-03-14 00:42:11 +0000

    args: add max_sequence_length and max_batch_size

--- args.py
+++ args.py
@@ -1,6 +1,8 @@
 class Args:
   head_count: int = 8
   input_dimension: int = 512
+  max_sequence_length: int = 2048
+  max_batch_size: int = 32
 
   # TODO: get rid of this unneeded `self`
   @property
